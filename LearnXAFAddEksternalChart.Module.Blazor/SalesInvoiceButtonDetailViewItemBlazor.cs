4ebd0aa1cde9f76e1ccbf54018201964fa37a9a0
tegarswasono <tegar.s@weefer.co.id>
2022-02-15 11:02:42 +0700

    add chart

---
+++ LearnXAFAddEksternalChart.Module.Blazor/SalesInvoiceButtonDetailViewItemBlazor.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnXAFAddEksternalChart.Module.Blazor
+{
+    public interface IModelSalesInvoiceButtonDetailViewItemBlazor : IModelViewItem { }
+
+    [ViewItem(typeof(IModelSalesInvoiceButtonDetailViewItemBlazor))]
+    public class SalesInvoiceButtonDetailViewItemBlazor : ViewItem
+    {
+        public class DxButtonHolder : IComponentContentHolder
+        {
+            RenderFragment IComponentContentHolder.ComponentContent => SalesInvoiceButtonRenderer.Create();
+        }
+        public SalesInvoiceButtonDetailViewItemBlazor(IModelViewItem model, Type objectType) : base(objectType, model.Id) { }
+        protected override object CreateControlCore() => new DxButtonHolder();
+    }
+}
---
+++ LearnXAFAddEksternalChart.Module/BusinessObjects/Order.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnXAFAddEksternalChart.Module.BusinessObjects
+{
+	[DefaultClassOptions]
+	public class Order : BaseObject
+	{
+		public Order(Session session) : base(session) { }
+		private ProductNameEnum _productName;
+		private double _price;
+		private DateTime _buyAt;
+		public ProductNameEnum ProductName
+		{
+			get { return _productName; }
+			set { SetPropertyValue(nameof(ProductName), ref _productName, value); }
+		}
+		public double Price
+		{
+			get { return _price; }
+			set { SetPropertyValue(nameof(Price), ref _price, value); }
+		}
+		public DateTime BuyAt
+		{
+			get { return _buyAt; }
+			set { SetPropertyValue(nameof(BuyAt), ref _buyAt, value); }
+		}
+	}
+	public enum ProductNameEnum
+    {
+		ChildrenClothes, WomenClothes, ManClothes
+    }
+}
